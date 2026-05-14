f8a44868e07959fbe37c152bc1c6b67d486769c8
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2026-05-14 13:58:09 +0530

    feat(worker): add document chunking

--- server/src/worker.js
+++ server/src/worker.js
@@ -1,12 +1,20 @@
 import { Worker } from "bullmq";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { QdrantVectorStore } from "@langchain/qdrant";
+import { Document } from "@langchain/core/documents";
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { CharacterTextSplitter } from "@langchain/textsplitters";
 
 const worker = new Worker(
   "file-upload-queue",
   async (job) => {
     console.log("job ", job.data);
+    console.log("job name", job.name);
 
-    if (job.name === "file-ready") {
-    }
+    const data = JSON.parse(job.data);
+    const loader = new PDFLoader(data.path);
+    const doc = await loader.load();
+    console.log("docs ", doc);
   },
   {
     concurrency: 100,
